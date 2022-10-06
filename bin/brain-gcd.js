6fb79599544bc139e3618a90ced7fc71633baf9b
Алексей Дом <promcity8@rambler.ru>
2022-10-06 15:29:00 +0700

    add gcd-game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import GcdGame from '../src/games/gcd-games.js';
+
+GcdGame();
---
+++ src/games/gcd-games.js
@@ -0,0 +1,18 @@
+import { randomNumber } from '../randomNumber.js';
+import game from '../index.js';
+
+const instruction = 'Find the greatest common divisor of given numbers.';
+
+const greatestCommonDivider = (x, y) => (y ? greatestCommonDivider(y, x % y) : x);
+
+const Round = () => {
+  const num1 = randomNumber(50);
+  const num2 = randomNumber(50);
+  const correctAnswer = String(greatestCommonDivider(num1, num2));
+  const question = `${num1} ${num2}`;
+  return { question, correctAnswer };
+};
+
+const GcdGame = () => game(Round, instruction);
+
+export default GcdGame;
